f6409ad3dfcab62c9c74ea9128753cf1a683b018
Cullen Lee <a410615903@gmail.com>
2021-09-13 18:42:40 -0700

    Roll backed Updated git subtree process

--- git_subtree_push.sh
+++ git_subtree_push.sh
@@ -1,1 +1,1 @@
-git subtree push --prefix packages/markdown_editor https://github.com/keyskull/markdown_editor.git master --squash
+git subtree push --prefix packages/markdown_editor https://github.com/keyskull/markdown_editor.git master
